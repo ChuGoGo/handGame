1acb46d77684c74d91db41360a9d2dacc19393a0
chugogo <ioueasonae86@yahoo.com.tw>
2022-02-24 14:02:24 +0800

    Initial Commit

---
+++ handGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  handGame
+//
+//  Created by Chu Go-Go on 2022/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
